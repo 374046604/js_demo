1dd354475ee9d5a908490c95cddc68d0815b636f
何三磊 <374046604@qq.com>
2021-06-10 14:03:48 +0800

    完善代码

--- src/components/Element.vue
+++ src/components/Element.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="element_box">
+    <el-button @click="testFun()">mixins的函数名冲突，以组价内部为准</el-button >
     <div class="element_li mar_b_20">
       <el-input v-model="test" disabled />
       <el-input v-model="test" clearable />
@@ -16,14 +17,24 @@
         show-password
         clearable
       ></el-input>
-      <my-input v-model="name" clearable/>
-      <my-input v-model="name" rows="2" type="textarea"/>
-      <input type="textarea"  class="textarea"/>
+      
+      <my-input v-model="name" disabled />
+      <my-input v-model="name" clearable />
+      <my-input v-model="name" showPassword />
+      <my-input v-model="name" icon="date_icon" />
+      <my-input v-model="name" rows="20" type="textarea" />
     </div>
   </div>
 </template>
 
 <script>
+let testMixins = {
+  methods: {
+    testFun(){
+      console.log('1111111')
+    }
+  }
+}
 export default {
   name: "element_components",
   data() {
@@ -32,8 +43,13 @@ export default {
       test: "22222",
     };
   },
+  mixins: [testMixins],
   mounted() {},
-  methods: {},
+  methods: {
+    testFun(){
+      console.log('2222222')
+    }
+  },
 };
 </script>
 
--- src/myElement/myInput.vue
+++ src/myElement/myInput.vue
@@ -2,22 +2,33 @@
   <div>
     <div class="my_input_box">
       <input
-				:type="showPassword ? (showPasswordVisible ? 'text': 'password') : type"
-        :class="`my_inner my_input ${disabled && 'is_disabled'} ${(type=='textarea'&& Number(rows)>1)&& 'is_textarea'}`"
+        v-if="type != 'textarea'"
+        :type="
+          showPassword ? (showPasswordVisible ? 'text' : 'password') : type
+        "
+        :class="`my_inner my_input ${disabled && 'is_disabled'}`"
         :disabled="disabled"
-				:value="value"
-      	v-on:change="$emit('change', $event.target.value)"
-				@input="inputFun"
-				:style="Number(rows)>1?`height:${30*Number(rows)}px;`:''"
+        :value="value"
+        v-on:change="$emit('change', $event.target.value)"
+        @input="inputFun"
       />
-			<div class="clear_icon">
-				<!-- 带图标的input -->
-				<div v-if="icon=='date_icon'">📅</div>
-				<!-- 带清除的input -->
-				<div v-if="showIcon" @click="clearInputFun">❎</div>
-				<!-- 带显示密码的input -->
-				<div v-if="showPassword" @click="showPasswordFun">✅</div>
-			</div>
+      <textarea
+        v-else
+        :class="`my_inner my_textarea ${
+          type == 'textarea' && Number(rows) > 1 && 'is_textarea'
+        }`"
+        :value="value"
+        v-on:change="$emit('change', $event.target.value)"
+        :style="Number(rows) > 1 ? `height:${30 * Number(rows)}px;line-height:1.5;` : ''"
+      ></textarea>
+      <div class="clear_icon">
+        <!-- 带清除的input -->
+        <div v-if="showIcon" @click="clearInputFun">❎</div>
+        <!-- 带图标的input -->
+        <div v-if="icon == 'date_icon'">📅</div>
+        <!-- 带显示密码的input -->
+        <div v-if="showPassword" @click="showPasswordFun">✅</div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,55 +36,54 @@
 <script>
 export default {
   name: "myInput",
-	//设置双向绑定  prop 取从父级得到的值  event 监听的回调事件
+  //设置双向绑定  prop 取从父级得到的值  event 监听的回调事件
   model: {
-    prop: 'value',
-    event: 'change'
+    prop: "value",
+    event: "change",
   },
   props: {
-    value:[String,Number],//绑定的值
-    disabled:{ //禁用
+    value: [String, Number], //绑定的值
+    disabled: {
+      //禁用
       type: Boolean,
       default: false,
     },
-    clearable: { //带删除按钮
+    clearable: {
+      //带删除按钮
       type: Boolean,
       default: false,
     },
-		showPassword:Boolean,//密码输入框
-		type:{
-			type:String,
-			default:'text'
-		},
-		icon:[String,Number],
-		rows:[String,Number]
+    showPassword: Boolean, //密码输入框
+    type: {
+      type: String,
+      default: "text",
+    },
+    icon: [String, Number],
+    rows: [String, Number],
   },
   data() {
     return {
-			showIcon:false,
-			showPasswordVisible:false,//控制显示按钮开关
+      showIcon: false,
+      showPasswordVisible: false, //控制显示按钮开关
     };
   },
   mounted() {
-		console.log(this.aaaa)
-		console.log(this.aaaa)
-		console.log(this.aaaa)
-	},
+  },
   methods: {
-		//控制清除按钮显示
-		inputFun(event){
-			this.showIcon = String(event.target.value).length?true:false;
-			this.$emit('change',event.target.value);
-		},
-		//清空数据
-		clearInputFun(){
-			this.showIcon = false;
-			this.$emit('change',null);
-		},
-		showPasswordFun(){
-			this.showPasswordVisible = !this.showPasswordVisible;
-		}
-	},
+    //控制清除按钮显示
+    inputFun(event) {
+      this.showIcon = String(event.target.value).length ? true : false;
+      this.$emit("change", event.target.value);
+    },
+    //清空数据
+    clearInputFun() {
+      this.showIcon = false;
+      this.$emit("change", null);
+    },
+    showPasswordFun() {
+      this.showPasswordVisible = !this.showPasswordVisible;
+    },
+  },
 };
 </script>
 
