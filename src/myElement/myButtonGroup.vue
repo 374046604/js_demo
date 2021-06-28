c1214869ad22b51e60676a06138556c23332e835
何三磊 <374046604@qq.com>
2021-06-28 17:24:43 +0800

    no message

---
+++ src/myElement/myButtonGroup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="my_button_group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "myButtonGroup"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../utils/element.scss";
+</style>
